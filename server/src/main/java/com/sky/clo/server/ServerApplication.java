6f23315fbe8623ce8e4723abce0b9ff3322ddc76
Marcin Grabarczyk <marcin.grabarczyk@sky.uk>
2020-10-08 17:05:05 +0100

    The server now returns the HTML page and a HTML file can be displayed

--- server/src/main/java/com/sky/clo/server/ServerApplication.java
+++ server/src/main/java/com/sky/clo/server/ServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +15,7 @@ public class ServerApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public ModelAndView hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new ModelAndView("demo.html");
 	}
 }
