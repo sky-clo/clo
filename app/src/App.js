d5f38bb5eeda5cb073d61a250cfa2c802525cbd8
Ryan Impey <ryan.impey@sky.uk>
2020-10-12 16:59:18 +0100

    Cleaned up css module imports for readability

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className={styles.app}>
       <header className={styles.app_header}>
-        <img src={logo} className={styles['app-logo']} alt="logo" />
+        <img src={logo} className={styles.app_logo} alt="logo" />
         <a
           className="c-btn c-btn--primary"
           href="https://reactjs.org"
