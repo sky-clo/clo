7916a00cb80d41f97fa85d21820ec79a9f9dfd31
Pav Sidhu <pavandeep.sidhu@sky.uk>
2020-10-13 17:02:39 +0100

    Remove app tests

--- app/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./index.scss";
-
-export default function App() {
-  return <main />;
-}
--- app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./App";
-import { shallow } from "enzyme";
-
-test("renders learn react link", () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper).toMatchSnapshot();
-});
