e2a2794068943945662a886dedd2a8e264267abc
Lindsey Wells <lindsey.wells@sky.uk>
2020-10-14 17:06:10 +0100

    add constructor to searchbar

--- app/src/SearchBar.js
+++ app/src/SearchBar.js
@@ -2,60 +2,59 @@ import React, { Component } from "react";
 import "./SearchBar.module.css";
 
 class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      from: "",
+      to: "",
+      date: "",
+    };
+  }
+
   render() {
     return (
       <form>
+        <label class="c-form-label" for="f-combo">
+          FROM
+        </label>
+        <div class="c-form-combo">
+          <div class="c-form-combo__cell">
+            <input
+              type="text"
+              class="c-form-combo__input c-form-input"
+              placeholder="e.g. London"
+              name="f-combo"
+              id="f-combo"
+            />
+          </div>
+        </div>
+
+        <label class="c-form-label" for="f-combo">
+          TO
+        </label>
+        <div class="c-form-combo">
+          <div class="c-form-combo__cell">
+            <input
+              type="text"
+              class="c-form-combo__input c-form-input"
+              placeholder="e.g. Croatia"
+              name="f-combo"
+              id="f-combo"
+            />
+          </div>
+        </div>
+
         <li class="c-form-list__item">
-          <label class="c-form-label" for="f-combo">
-            FROM
-          </label>
-          <div class="c-form-combo">
-            <div class="c-form-combo__cell">
-              <input
-                type="text"
-                class="c-form-combo__input c-form-input"
-                placeholder="e.g. London"
-                name="f-combo"
-                id="f-combo"
-              />
-            </div>
-          </div>
-
-          <label class="c-form-label" for="f-combo">
-            TO
-          </label>
-          <div class="c-form-combo">
-            <div class="c-form-combo__cell">
-              <input
-                type="text"
-                class="c-form-combo__input c-form-input"
-                placeholder="e.g. Croatia"
-                name="f-combo"
-                id="f-combo"
-              />
-            </div>
-          </div>
-
-          <label class="c-form-label" for="f-combo">
+          <label class="c-form-label" for="f-date">
             DATE
           </label>
-          <div class="c-form-combo">
-            <div class="c-form-combo__cell">
-              <input
-                type="text"
-                class="c-form-combo__input c-form-input"
-                placeholder="e.g. 08/08/2020"
-                name="f-combo"
-                id="f-combo"
-              />
-            </div>
-
-            <div class="c-form-combo__cell">
-              <button class="c-form-combo__btn c-btn c-btn--primary">
-                Search
-              </button>
-            </div>
-          </div>
+          <input
+            type="date"
+            class="c-form-date"
+            placeholder="dd/mm/yyyy"
+            name="f-date"
+            id="f-date"
+          />
         </li>
       </form>
     );
