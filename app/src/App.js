5ebf042460c481010acf47856eb7fccdea205967
Pav Sidhu <pavandeep.sidhu@sky.uk>
2020-10-14 16:18:38 +0100

    Add project structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Location from "./pages/location/Location";
+import styles from "./App.module.css";
 
 export default function App() {
-  return <main />;
+  return (
+    <div className={styles.container}>
+      <header>Header</header>
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/location" component={Location} />
+          </Switch>
+        </Router>
+      </main>
+      <footer>Footer</footer>
+    </div>
+  );
 }
