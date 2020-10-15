018b3f03fdd4979e5b14987c1e966434bc77f934
Ryan Impey <ryan.impey@sky.uk>
2020-10-15 16:34:58 +0100

    Replaced string concat with uriParams Map

--- server/src/main/java/com/sky/clo/weather/WeatherController.java
+++ server/src/main/java/com/sky/clo/weather/WeatherController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WeatherController {
@@ -12,10 +16,15 @@ public class WeatherController {
     private String weatherApiKey;
 
     @GetMapping("/weather")
-    public Weather weather(@RequestParam(value = "location", defaultValue = "London") String location) {
+    public Weather weatherRequest(@RequestParam(value = "location", defaultValue = "London") String location) throws RestClientException {
         RestTemplate template = new RestTemplate();
-        String url = "https://api.weatherapi.com/v1/current.json?key=" + weatherApiKey + "&q=" + location;
+        Map<String, String> uriParams = new HashMap<String, String>();
 
-        return template.getForObject(url, Weather.class);
+        String url = "https://api.weatherapi.com/v1/current.json?key={apiKey}&q={query}";
+        uriParams.put("apiKey", weatherApiKey);
+        uriParams.put("query", location);
+
+        Weather weatherResponse = template.getForObject(url, Weather.class, uriParams);
+        return weatherResponse;
     }
 }
