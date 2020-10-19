12d3a239b3f3e7423d116fadb9e05511d56bfa5b
Pav Sidhu <pavandeep.sidhu@sky.uk>
2020-10-19 11:23:23 +0100

    Add props to hero component and titles to pages

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import Hero from "../../components/hero/Hero";
 import SearchBar from "../../components/searchBar/SearchBar";
 import LocationCard from "../../components/locationCard/LocationCard";
+import heroImage from "../../images/hero.png";
 import styles from "./home.module.scss";
 
 class Location {
@@ -19,19 +21,25 @@ export default function Home() {
   }
 
   return (
-    <article className={styles.home}>
-      <Hero />
-      <SearchBar />
-      <section className={styles.locations}>
-        <div className="o-container">
-          <h2 className="c-heading-bravo">Popular Locations</h2>
-          <div className={"o-layout " + styles.locationCards}>
-            {locations.map((location, index) => (
-              <LocationCard location={location} key={index} />
-            ))}
+    <>
+      <Helmet>
+        <title>Home | Sky Clo</title>
+      </Helmet>
+
+      <article className={styles.home}>
+        <Hero title="Where would you like to go?" image={heroImage} />
+        <SearchBar />
+        <section className={styles.locations}>
+          <div className="o-container">
+            <h2 className="c-heading-bravo">Popular Locations</h2>
+            <div className={"o-layout " + styles.locationCards}>
+              {locations.map((location, index) => (
+                <LocationCard location={location} key={index} />
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
-    </article>
+        </section>
+      </article>
+    </>
   );
 }
