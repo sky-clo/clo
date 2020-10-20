338a946c03301ba9c23ef6931b9e8d03b96fad68
Ryan Impey <ryan.impey@sky.uk>
2020-10-20 12:03:59 +0100

    Added files related to Airport retrieval

--- server/src/main/java/com/sky/clo/db/Airport.java
+++ server/src/main/java/com/sky/clo/db/Airport.java
@@ -1,2 +1,92 @@
-package com.sky.clo.db;public class Airport {
+package com.sky.clo.db;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "airports")
+public class Airport {
+    @Id
+    private String code;
+    private float lat;
+    private float lon;
+    private String title;
+    private String city;
+    private String state;
+    private String country;
+    private int woeid;
+    private String tz;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getWoeid() {
+        return woeid;
+    }
+
+    public void setWoeid(int woeid) {
+        this.woeid = woeid;
+    }
+
+    public String getTz() {
+        return tz;
+    }
+
+    public void setTz(String tz) {
+        this.tz = tz;
+    }
 }
--- server/src/main/java/com/sky/clo/db/AirportRepository.java
+++ server/src/main/java/com/sky/clo/db/AirportRepository.java
@@ -1,2 +1,16 @@
-package com.sky.clo.db;public class WeatherRepository {
+package com.sky.clo.db;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public interface AirportRepository extends CrudRepository<Airport, Integer> {
+    @Query("SELECT a FROM Airport a WHERE a.code LIKE %:query% OR a.title LIKE %:query% OR a.city LIKE %:query% OR a.state LIKE %:query% OR a.country LIKE %:query%")
+    List<Airport> findAny(@Param("query") String query);
 }
