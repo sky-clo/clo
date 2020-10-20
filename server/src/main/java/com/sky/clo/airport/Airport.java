8cdb03e4f6ae803b14f1c12a578cdb45eb2c7991
Ryan Impey <ryan.impey@sky.uk>
2020-10-20 16:00:00 +0100

    Modified Airport files + controllers to work with RapidAPI

--- server/src/main/java/com/sky/clo/airport/Airport.java
+++ server/src/main/java/com/sky/clo/airport/Airport.java
@@ -1,2 +1,15 @@
-package com.sky.clo.airport;public class Airport {
+package com.sky.clo.airport;
+
+import java.util.Collection;
+
+public class Airport {
+    private Collection<Places> Places;
+
+    public Collection<com.sky.clo.airport.Places> getPlaces() {
+        return Places;
+    }
+
+    public void setPlaces(Collection<com.sky.clo.airport.Places> places) {
+        Places = places;
+    }
 }
--- server/src/main/java/com/sky/clo/airport/Places.java
+++ server/src/main/java/com/sky/clo/airport/Places.java
@@ -1,2 +1,60 @@
-package com.sky.clo.airport;public class Places {
+package com.sky.clo.airport;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Places {
+    private String PlaceId;
+    private String PlaceName;
+    private String CountryId;
+    private String RegionId;
+    private String CityId;
+    private String CountryName;
+
+    public String getPlaceId() {
+        return this.PlaceId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.PlaceId = placeId;
+    }
+
+    public String getPlaceName() {
+        return PlaceName;
+    }
+
+    public void setPlaceName(String placeName) {
+        PlaceName = placeName;
+    }
+
+    public String getCountryId() {
+        return CountryId;
+    }
+
+    public void setCountryId(String countryId) {
+        CountryId = countryId;
+    }
+
+    public String getRegionId() {
+        return RegionId;
+    }
+
+    public void setRegionId(String regionId) {
+        RegionId = regionId;
+    }
+
+    public String getCityId() {
+        return CityId;
+    }
+
+    public void setCityId(String cityId) {
+        CityId = cityId;
+    }
+
+    public String getCountryName() {
+        return CountryName;
+    }
+
+    public void setCountryName(String countryName) {
+        CountryName = countryName;
+    }
 }
