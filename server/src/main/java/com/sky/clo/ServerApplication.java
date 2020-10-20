828e178d703abbac2cf7911a7a7d83f8cd329e1f
Ryan Impey <ryan.impey@sky.uk>
2020-10-20 16:00:32 +0100

    Added AirportController class to Spring basePackageClasses

--- server/src/main/java/com/sky/clo/ServerApplication.java
+++ server/src/main/java/com/sky/clo/ServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 // Initialises and runs the server application
 // Any external classes used for REST/Controllers should be added to the basePackageClasses list
-@SpringBootApplication @ComponentScan(basePackageClasses = {WeatherController.class})
+@SpringBootApplication @ComponentScan(basePackageClasses = {WeatherController.class, AirportController.class})
 public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
