e45c2b3e9e27845efa86ae06f4d724ea9066522b
Ryan Impey <ryan.impey@sky.uk>
2020-10-26 17:14:22 +0000

    Added react-hook-form and updated validation system

--- app/src/pages/createAnAccount/CreateAnAccount.js
+++ app/src/pages/createAnAccount/CreateAnAccount.js
@@ -3,6 +3,7 @@ import styles from "./createAnAccount.module.css";
 import Button from "../../components/button/Button";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function CreateAnAccount() {
   const [firstName, setFirstName] = useState("");
@@ -12,136 +13,161 @@ export default function CreateAnAccount() {
   const [houseNumber, setHouseNumber] = useState("");
   const [postcode, setPostcode] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(
-      `Thanks for signing you up with these details....${firstName} ${lastName} ${email} ${houseNumber} ${postcode}`
-    ); //just a placeholder for our post request
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const onSubmit = (data) => {
+    //e.preventDefault();
+    console.log(data);
+    alert(`Thanks for signing you up with these details`); //just a placeholder for our post request
   };
 
   return (
     <section className={styles.section}>
       <h1 className={styles.h1}>Create an Account</h1>
 
-      <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <legend className="c-form-caption">Example</legend>
           <ul className={"c-form-list " + styles.formList}>
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-firstname">
-                First name{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-firstname">
+                First name
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
               <input
+                ref={register()}
                 type="text"
                 className="c-form-input"
                 placeholder="e.g. Joe"
                 name="f-firstname"
                 id="f-firstname"
-                onChange={(e) => setFirstName(e.target.value)}
-                value={firstName}
+                //onChange={(e) => setFirstName(e.target.value)}
+                //value={firstName}
                 data-test="CreateAnAccount-first-name"
                 required
               />
             </li>
 
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-lastname">
-                Last name{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-lastname">
+                Last name
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
               <input
+                ref={register()}
                 type="text"
                 className="c-form-input"
                 placeholder="e.g. Smith"
                 name="f-lastname"
                 id="f-lastname"
-                onChange={(e) => setLastName(e.target.value)}
-                value={lastName}
+                //onChange={(e) => setLastName(e.target.value)}
+                //value={lastName}
                 data-test="CreateAnAccount-last-name"
                 required
               />
             </li>
 
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-email">
-                Email{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-email">
+                Email
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
               <input
+                ref={register()}
                 type="email"
                 className="c-form-input"
                 placeholder="e.g. joe@bloggs.com"
                 name="f-email"
                 id="f-email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
+                //onChange={(e) => setEmail(e.target.value)}
+                //value={email}
                 data-test="CreateAnAccount-email"
                 required
               />
             </li>
 
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-password">
-                Password{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-password">
+                Password
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
               <input
+                ref={register()}
                 type="password"
                 className="c-form-input"
                 placeholder="*****"
                 name="f-password"
                 id="f-password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
+                //onChange={(e) => setPassword(e.target.value)}
+                //value={password}
                 data-test="CreateAnAccount-password"
                 required
               />
             </li>
 
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-housenumber">
-                House Number / Name{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-housenumber">
+                House Number / Name
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
               <input
+                ref={register()}
                 type="text"
                 className="c-form-input"
                 placeholder="e.g. 1 Grant Way"
                 name="f-housenumber"
                 id="f-housenumber"
-                onChange={(e) => setHouseNumber(e.target.value)}
-                value={houseNumber}
+                //onChange={(e) => setHouseNumber(e.target.value)}
+                //value={houseNumber}
                 data-test="CreateAnAccount-house-number"
                 required
               />
             </li>
 
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-postcode">
-                Postcode{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-postcode">
+                Postcode
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
               <input
+                ref={register()}
                 type="text"
                 className="c-form-input"
                 placeholder="e.g. SW1W 0NY"
                 name="f-postcode"
                 id="f-postcode"
-                onChange={(e) => setPostcode(e.target.value)}
-                value={postcode}
+                //onChange={(e) => setPostcode(e.target.value)}
+                //value={postcode}
                 data-test="CreateAnAccount-postcode"
                 required
               />
--- app/src/pages/signIn/SignIn.js
+++ app/src/pages/signIn/SignIn.js
@@ -3,28 +3,28 @@ import styles from "./signIn.module.css";
 import Button from "../../components/button/Button";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Thanks for logging in ${email}`); //just a placeholder for our post request
-  };
+  //const [email, setEmail] = useState("");
+  //const [password, setPassword] = useState("");
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   return (
     <section className={styles.section}>
       <h1 className={styles.h1}>Sign In</h1>
-
-      <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <legend className="c-form-caption">Example</legend>
           <ul className={"c-form-list " + styles.formList}>
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-email">
-                Email{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-email">
+                Email
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
@@ -34,17 +34,21 @@ export default function SignIn() {
                 placeholder="e.g. joe@bloggs.com"
                 name="f-email"
                 id="f-email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
+                ref={register({ required: true })}
+                //onChange={(e) => setEmail(e.target.value)}
+                //value={email}
                 data-test="SignIn-email"
                 required
               />
             </li>
 
             <li className="c-form-list__item">
-              <label className="c-form-label" for="f-password">
-                Password{" "}
-                <abbr title="This field is required" class="c-form-required">
+              <label className="c-form-label" htmlFor="f-password">
+                Password
+                <abbr
+                  title="This field is required"
+                  className="c-form-required"
+                >
                   *
                 </abbr>
               </label>
@@ -54,17 +58,17 @@ export default function SignIn() {
                 placeholder="*****"
                 name="f-password"
                 id="f-password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
+                ref={register({ required: true })}
+                //onChange={(e) => setPassword(e.target.value)}
+                //value={password}
                 data-test="SignIn-password"
                 required
               />
             </li>
-
             <Button data-test="SignIn-next">Next</Button>
           </ul>
         </fieldset>
-        <Link to={`/create-an-account`} data-test="SignIn-create-an-account">
+        <Link to="/create-an-account" data-test="SignIn-create-an-account">
           Or create an account
         </Link>
       </form>
