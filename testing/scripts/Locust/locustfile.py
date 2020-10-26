c710a5b60b2821e8c3bec1c7c6b879844fc5739a
Marcin Grabarczyk <marcin.grabarczyk@sky.uk>
2020-10-26 14:26:45 +0000

    Added some extra locust tests

--- testing/scripts/Locust/locustfile.py
+++ testing/scripts/Locust/locustfile.py
@@ -1,13 +1,23 @@
 import time
 from locust import HttpUser, task, between
 
-class QuickstartUser(HttpUser):
-	wait_time = between(1,2 )
+class BrowserUser(HttpUser):
+	wait_time = between(1,2)
 
-	@task
+	@task(10)
 	def index_page(self):
 		self.client.get("")
 
-	@task
-	def demo_age(self):
-		self.client.get("demo")
+	@task(6)
+	def sign_in_page(self):
+		self.client.get("sign-in")
+
+	@task(3)
+	def create_an_account_page(self):
+		self.client.get("create-an-account")
+
+	@task(2)
+	def get_location_page(self):
+		list_of_cities = ["London", "Split", "Moscow", "Madrid"]
+		for city in list_of_cities:
+			self.client.get("location/%s" % city)
