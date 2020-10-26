0ce1fbf44c32e8033236b31b39ac3b1f93ad1dcc
Ryan Impey <ryan.impey@sky.uk>
2020-10-26 09:23:52 +0000

    Added user details to auth response

--- server/src/main/java/com/sky/clo/models/AuthenticationResponse.java
+++ server/src/main/java/com/sky/clo/models/AuthenticationResponse.java
@@ -1,13 +1,32 @@
 package com.sky.clo.models;
 
+import com.sky.clo.db.User;
+
 public class AuthenticationResponse {
     private final String jwt;
+    private final User user;
 
     public AuthenticationResponse(String jwt) {
         this.jwt = jwt;
+        this.user = null;
+    }
+
+    public AuthenticationResponse(String jwt, User user) {
+        this.jwt = jwt;
+        this.user = user;
     }
 
     public String getJwt() {
         return jwt;
+    }
+
+    public UserResponse getUser() {
+        return new UserResponse(
+                this.user.getFirstname(),
+                this.user.getLastname(),
+                this.user.getEmail(),
+                this.user.getHouse_no(),
+                this.user.getPostcode()
+        );
     }
 }
