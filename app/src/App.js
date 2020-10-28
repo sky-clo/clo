37fedecb216deeff4ae601a9684160021e041365
Ryan Impey <ryan.impey@sky.uk>
2020-10-28 14:39:53 +0000

    Added AuthContext.Provider to App.js

--- app/src/App.js
+++ app/src/App.js
@@ -16,24 +16,26 @@ export default function App() {
   const [auth, dispatch] = useReducer(authReducer, getInitialAuthState());
 
   return (
-    <Router>
-      <div className={styles.container}>
-        <Header />
-        <main>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/search" component={Search} />
-            <Route path="/create-an-account" component={CreateAnAccount} />
-            <Route path="/sign-in" component={SignIn} />
-            <Route path="/whos-flying" component={WhosFlying} />
-            <Route path="/payment" component={Payment} />
-            <Route path="/complete" component={CompleteTrip} />
-          </Switch>
-        </main>
-        <footer className="o-container">
-          © {new Date().getFullYear()} Sky UK
-        </footer>
-      </div>
-    </Router>
+    <AuthContext.Provider value={{ auth, dispatch }}>
+      <Router>
+        <div className={styles.container}>
+          <Header />
+          <main>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/search" component={Search} />
+              <Route path="/create-an-account" component={CreateAnAccount} />
+              <Route path="/sign-in" component={SignIn} />
+              <Route path="/whos-flying" component={WhosFlying} />
+              <Route path="/payment" component={Payment} />
+              <Route path="/complete" component={CompleteTrip} />
+            </Switch>
+          </main>
+          <footer className="o-container">
+            © {new Date().getFullYear()} Sky UK
+          </footer>
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
