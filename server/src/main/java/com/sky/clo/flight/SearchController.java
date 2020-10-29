74dea5637d503246fbf12737ac1be0640f3bc8bb
David Hoskins <David.Hoskins@sky.uk>
2020-10-29 11:14:50 +0000

    updating search controller to correct format

--- server/src/main/java/com/sky/clo/flight/SearchController.java
+++ server/src/main/java/com/sky/clo/flight/SearchController.java
@@ -1,14 +1,13 @@
 package com.sky.clo.flight;
 
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,14 +17,18 @@ import java.util.Map;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RestController
+@Controller
+@RequestMapping(path = "/search")
 public class SearchController {
     @Value("${com.rapidapi.key}")
     private String rapidApiKey;
 
-    @GetMapping("/search")
-    public JsonNode searchRequest(@RequestParam String from, @RequestParam String to, @RequestParam String outboundDate,
-            @RequestParam(required = false) String inboundDate) throws RestClientException {
+    @GetMapping(path = { "/", "" })
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public JsonNode searchRequest(@RequestParam(value = "from") String from, @RequestParam(value = "to") String to,
+            @RequestParam(value = "outboundDate") String outboundDate,
+            @RequestParam(required = false, value = "inboundDate") String inboundDate) throws RestClientException {
         RestTemplate template = new RestTemplate();
         Map<String, String> uriParams = new HashMap<String, String>();
 
